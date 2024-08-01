052fef5bfa2c9772e57c40bc02c7db05e159946b
marlon-figuerpa <marlon.f.1993@gmail.com>
2024-08-01 16:36:55 -0600

    creacion de categoria

--- siemprendo/src/main/java/org/primefaces/diamond/service/IService.java
+++ siemprendo/src/main/java/org/primefaces/diamond/service/IService.java
@@ -1,7 +1,17 @@
 package org.primefaces.diamond.service;
 
+import java.util.List;
+
 public interface IService<T> {
 
     boolean create(T entity);
+
+    boolean edit(T entity);
+
+    boolean remove(T entity);
+
+    List<T> findAll();
+
+    T findById(int id);
 
 }
